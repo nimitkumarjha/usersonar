e7ea79bd71f0251e3e592a9ff67a523590f5124b
nimitkumarjha <65605059+nimitkumarjha@users.noreply.github.com>
2020-06-18 13:16:16 +0530

    Update EcommerceUserManagementApplication.java

--- src/main/java/com/infy/EcommerceUserManagement/EcommerceUserManagementApplication.java
+++ src/main/java/com/infy/EcommerceUserManagement/EcommerceUserManagementApplication.java
@@ -3,14 +3,12 @@ package com.infy.EcommerceUserManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan
-@EnableDiscoveryClient
 public class EcommerceUserManagementApplication {
 
     	public static void main(String[] args) {
